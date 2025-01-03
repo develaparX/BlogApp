b2b6a839efaa88ce92aeb74ff39dfef986a030e5
arunikax <srarfian@gmail.com>
2025-01-03 14:35:38 +0700

    using imageKitIo for images, install react-router-dom, add routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Image from "./Image";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -7,35 +9,51 @@ const Navbar = () => {
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
       <div className="flex items-center gap-4 text-2xl font-bold">
-        <img src="/logo.png" className="w-8 h-8" alt="" />
+        <Image
+          src="logo.png"
+          alt="Develapar Blog"
+          // className="w-8 h-8"
+          w={32}
+          h={32}
+        />
         <span>.develapar</span>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
         {/* MOBILE BUTTOn */}
         <div
-          className="cursor-pointer text-2xl"
+          className="cursor-pointer text-4xl"
           onClick={() => setOpen((prev) => !prev)}
         >
           {open ? "X" : "="}
         </div>
         {/* MOBILE LINK LIST */}
         <div
-          className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 transition-all ease-in-out ${
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
             open ? "-right-0" : "-right-[100%]"
           } `}
         >
-          menu
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
         </div>
       </div>
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
         <a href="/">Home</a>
         <a href="/">Trending</a>
         <a href="/">Most Popular</a>
         <a href="/">About</a>
         <a href="">
-          <button>Login </button>
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
         </a>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import Homepage from "./routes/Homepage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import WritePage from "./routes/Write.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/posts",
+    element: <PostListPage />,
+  },
+  {
+    path: "/:slug",
+    element: <SinglePostPage />,
+  },
+  {
+    path: "/write",
+    element: <WritePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
