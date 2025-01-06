7cf9d156f4c66326488cfd2e156bd20c11b9610c
arunikax <srarfian@gmail.com>
2025-01-06 18:09:04 +0700

    setup mongoDB as Starting Database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/posts.route.js";
+import commentRouter from "./routes/comments.route.js";
+import connectDB from "./lib/connectDB.js";
 
 const app = express();
 
 app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 app.listen(3020, () => {
+  connectDB();
   console.log("server is running!");
 });
