005229230542cc52344b79b98a7402a9be74056e
arunikax <srarfian@gmail.com>
2025-01-07 22:59:25 +0700

    Add Clerk Users to Database & Clerk Authorization Middleware using @clerk/express

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,50 @@ import express from "express";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/posts.route.js";
 import commentRouter from "./routes/comments.route.js";
+import webHookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+
 import connectDB from "./lib/connectDB.js";
 
 const app = express();
+app.use(clerkMiddleware());
+app.use("/webhooks", webHookRouter);
+
 app.use(express.json());
+
+// app.get("/auth-state", (req, res) => {
+//   const authState = req.auth;
+//   res.json(authState);
+// });
+
+// app.get("/protect", (req, res) => {
+//   const { userId } = req.auth;
+
+//   if (!userId) {
+//     return res.status(401).json("not authenticated");
+//   }
+
+//   res.status(200).json("content");
+// });
+
+// app.get("/protect2", requireAuth(), (req, res) => {
+//   res.status(200).json("content");
+// });
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+//error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.listen(3020, () => {
   connectDB();
