ac18383d052af81cfc9b3fe97599b08becddd29e
arunikax <srarfian@gmail.com>
2025-01-11 15:14:00 +0700

    data mutation with react query and use react-toastify for notification icon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,8 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -61,6 +63,7 @@ createRoot(document.getElementById("root")).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" />
       </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>
--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -1,9 +1,33 @@
-import { useUser } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import "react-quill-new/dist/quill.snow.css";
 import ReactQuill from "react-quill-new";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const WritePage = () => {
   const { isLoaded, isSignedIn } = useUser();
+  const [value, setValue] = useState("");
+  const { getToken } = useAuth();
+
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: async (newPost) => {
+      const token = await getToken();
+      return axios.post(`${import.meta.env.VITE_API_URL}/posts`, newPost, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: (res) => {
+      toast.success("Post has been created");
+      navigate(`/${res.data.slug}`);
+    },
+  });
 
   if (!isLoaded) {
     return <div className="">Loading....</div>;
@@ -13,10 +37,26 @@ const WritePage = () => {
     return <div className="">You should login!</div>;
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      title: formData.get("title"),
+      category: formData.get("category"),
+      desc: formData.get("desc"),
+      content: value,
+    };
+
+    console.log(data);
+
+    mutation.mutate(data);
+  };
+
   return (
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
       <h1 className="text-xl font-light">Create a New Post</h1>
-      <form className="flex flex-col gap-6 flex-1 mb-6">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
         <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
           Add a cover image
         </button>
@@ -24,13 +64,14 @@ const WritePage = () => {
           className="text-4xl font-semibold bg-transparent outline-none"
           type="text"
           placeholder="My Awesome Story"
+          name="title"
         />
         <div className="flex items-center gap-4">
           <label htmlFor="" className="text-sm">
             Choose a category:
           </label>
           <select
-            name="cat"
+            name="category"
             id=""
             className="p-2 rounded-xl bg-white shadow-md"
           >
@@ -50,10 +91,16 @@ const WritePage = () => {
         <ReactQuill
           theme="snow"
           className="flex-1 rounded-xl bg-white shadow-md"
+          value={value}
+          onChange={setValue}
         />
-        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 ">
-          Send
+        <button
+          disabled={mutation.isPending}
+          className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed"
+        >
+          {mutation.isPending ? "Loading...." : "Send"}
         </button>
+        {mutation.isError && <span>{mutation.error.message}</span>}
       </form>
     </div>
   );
